ed364ff11208bf6546ed48094141b8f6de89d573
aidanwllmsn <ajwill1022@gmail.com>
2025-01-28 21:43:32 -0800

    Added Bio Card and Portrait

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
 import Navbar from "./components/navbar";
+import BioCard from "./components/BioCard";
+
 export default function Home() {
   return (
-    <Navbar/>
+    <div className="px-4 text-lg">
+      <Navbar/>
+      <div className="max-w-[50rem] mx-auto mt-8">
+        <div className="w-96 h-96 mx-auto mb-12 rounded-2xl overflow-hidden">
+          <img src="Portrait.jpg" alt="Picture of me" className="w-full h-full object-cover" />
+        </div>
+        <BioCard
+        name="Aidan Williamson"
+        email="ajwill1022@gmail.com"
+        github="https://github.com/aidanwllmsn"
+        linkedin="https://www.linkedin.com/in/aidan-williamson"
+        location="Los Angeles, CA"
+        />
+      </div>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        buttonhover: 'var(--buttonhover)',
+        buttonactive: 'var(--buttonactive)',
+        buttonactivehover: 'var(--buttonactivehover)',  
+      },
       fontFamily: {
         mono: ['Menlo', 'Consolas', 'Monaco', 'Liberation Mono', 'Lucida Console', 'monospace'],
+        sans: ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
       },
     },
   },
